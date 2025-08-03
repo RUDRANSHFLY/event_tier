6d9c5d89c66eb76010500c10748796942b833117
RUDRANSHFLY <mayamayavir@gmail.com>
2025-08-03 10:15:22 +0530

    feat : Remove Un-use buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { type Metadata } from 'next'
+import { type Metadata } from "next";
 import {
   ClerkProvider,
   SignInButton,
@@ -6,41 +6,46 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+} from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Event-Tier',
-  description: 'An event tier based on it',
-}
+  title: "Event-Tier",
+  description: "An event tier based on it",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
+              <SignInButton>
+                <Button variant={"ghost"} className="cursor-pointer">
+                  Sign In
+                </Button>
+              </SignInButton>
+              <SignUpButton forceRedirectUrl={"/auth/on-boarding"}>
+                <Button className="cursor-pointer">Sign Up</Button>
               </SignUpButton>
             </SignedOut>
             <SignedIn>
@@ -51,5 +56,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
